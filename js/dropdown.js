29c0891820ec594036b860f33173a04a4172cae0
RandomBrick613 <zepi613@gmail.com>
2026-07-04 11:24:20 +0100

    overhaul 2

---
+++ js/dropdown.js
@@ -0,0 +1,18 @@
+// Thank you W3Schools
+
+function myFunction() {
+  document.getElementById("nav-dropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
